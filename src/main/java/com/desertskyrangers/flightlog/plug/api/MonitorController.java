2e2f3a46c57e42b2aa78deeae5ed85275f87f787
msoderquist <msoderquist@mastercontrol.com>
2021-12-17 13:37:22 -0700

    Improve monitor api

--- src/main/java/com/desertskyrangers/flightlog/plug/api/MonitorController.java
+++ src/main/java/com/desertskyrangers/flightlog/plug/api/MonitorController.java
@@ -1,18 +1,25 @@
 package com.desertskyrangers.flightlog.plug.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/monitor")
+@RequestMapping( "/api/monitor" )
 public class MonitorController {
 
-	@GetMapping("/status")
-	public Map<String,String> isAlive() {
-		return Map.of( "running", "true");
+	@Value("${spring.application.version:unknown}") String version;
+
+	@GetMapping( "/status" )
+	public Map<String, String> isAlive() {
+		Map<String, String> response = new HashMap<>();
+		response.put( "running", "true" );
+		response.put( "version", version );
+		return response;
 	}
 
 }
