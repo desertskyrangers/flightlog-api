e977e7ca68411855312b7ae8063eb82a946ebb47
msoderquist <msoderquist@mastercontrol.com>
2021-12-17 13:40:29 -0700

    Improve application test

--- src/test/java/com/desertskyrangers/flightlog/FlightlogAppTests.java
+++ src/test/java/com/desertskyrangers/flightlog/FlightlogAppTests.java
@@ -2,6 +2,7 @@ package com.desertskyrangers.flightlog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
@@ -10,6 +11,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class FlightlogAppTests {
 
+	@Value("${spring.application.version:unknown}") String version;
+
 	@Autowired
 	ApplicationContext context;
 
@@ -17,6 +20,7 @@ class FlightlogAppTests {
 	void contextLoads() {
 		assertThat( context ).isNotNull();
 		assertThat( context.getEnvironment().getProperty( "server.port" ) ).isEqualTo( "8050" );
+		assertThat( context.getEnvironment().getProperty( "spring.application.version" ) ).isEqualTo( version ) ;
 	}
 
 }
