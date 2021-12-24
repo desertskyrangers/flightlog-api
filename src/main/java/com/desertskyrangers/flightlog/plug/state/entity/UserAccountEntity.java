5356064b7c78e5c92866c8f57ad372df66cfd3bf
Mark Soderquist <mark@soderquist.net>
2021-12-24 10:09:59 -0700

    Fix database generation

--- src/main/java/com/desertskyrangers/flightlog/plug/state/entity/UserAccountEntity.java
+++ src/main/java/com/desertskyrangers/flightlog/plug/state/entity/UserAccountEntity.java
@@ -11,7 +11,7 @@ public class UserAccountEntity {
 
 	@Id
 	@GeneratedValue( strategy = GenerationType.IDENTITY, generator = "native" )
-	private Long id;
+	private Integer id;
 
 	private String username;
 
