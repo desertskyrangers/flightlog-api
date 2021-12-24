410607af46c927e6d4141b68b4a75e007c25a0c2
Mark Soderquist <mark@soderquist.net>
2021-12-23 21:15:31 -0700

    Work on signup function

--- src/main/java/com/desertskyrangers/flightlog/plug/api/AuthController.java
+++ src/main/java/com/desertskyrangers/flightlog/plug/api/AuthController.java
@@ -2,14 +2,17 @@ package com.desertskyrangers.flightlog.plug.api;
 
 import com.desertskyrangers.flightlog.core.model.UserAccount;
 import com.desertskyrangers.flightlog.port.AuthRequesting;
-import com.desertskyrangers.flightlog.util.Json;
 import com.desertskyrangers.flightlog.util.Text;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class AuthController {
@@ -21,27 +24,33 @@ public class AuthController {
 	}
 
 	@PostMapping( path = ApiPath.AUTH_SIGNUP, consumes = "application/json", produces = "application/json" )
-	@ResponseStatus( HttpStatus.ACCEPTED )
 	@CrossOrigin( origins = "http://localhost:3000" )
-	void signup( @RequestBody ReactUserAccount request ) {
+	ResponseEntity<Map<String, Object>> signup( @RequestBody ReactUserAccount request ) {
 		List<String> messages = new ArrayList<>();
 		if( Text.isBlank( request.getUsername() ) ) messages.add( "Username required" );
 		if( Text.isBlank( request.getPassword() ) ) messages.add( "Password required" );
 		if( Text.isBlank( request.getEmail() ) ) messages.add( "EmailRequired" );
-		if( !messages.isEmpty() ) throw new ResponseStatusException( HttpStatus.BAD_REQUEST, Json.stringify( messages ) );
+		if( !messages.isEmpty() ) {
+			return new ResponseEntity<>( Map.of( "messages", messages ), HttpStatus.BAD_REQUEST );
+			//throw new ResponseStatusException( HttpStatus.BAD_REQUEST, Json.stringify( messages ) );
+		}
 		authRequesting.requestUserAccountSignup( new UserAccount().username( request.getUsername() ).password( request.getPassword() ).email( request.getEmail() ) );
+		return new ResponseEntity<>( Map.of(), HttpStatus.ACCEPTED );
 	}
 
 	@PostMapping( path = ApiPath.AUTH_LOGIN, consumes = "application/json", produces = "application/json" )
-	@ResponseStatus( HttpStatus.OK )
 	@CrossOrigin( origins = "http://localhost:3000" )
-	void login( @RequestBody ReactUserAccount request ) {
+	ResponseEntity<Map<String, Object>> login( @RequestBody ReactUserAccount request ) {
 		System.out.println( "login request username=" + request.getUsername() );
 		List<String> messages = new ArrayList<>();
 		if( Text.isBlank( request.getUsername() ) ) messages.add( "Username required" );
 		if( Text.isBlank( request.getPassword() ) ) messages.add( "Password required" );
-		if( !messages.isEmpty() ) throw new ResponseStatusException( HttpStatus.BAD_REQUEST, Json.stringify( messages ) );
+		if( !messages.isEmpty() ) {
+			return new ResponseEntity<>( Map.of( "messages", messages ), HttpStatus.BAD_REQUEST );
+			//throw new ResponseStatusException( HttpStatus.BAD_REQUEST, "Got some errors!" );
+		}
 		//authRequesting.requestUserAccountSignup( new UserAccount().username( request.getUsername() ).password( request.getPassword() ).email( request.getEmail() ) );
+		return new ResponseEntity<>( Map.of(), HttpStatus.OK );
 	}
 
 }
