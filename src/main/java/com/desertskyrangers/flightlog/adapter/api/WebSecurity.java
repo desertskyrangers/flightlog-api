b0053ae583f7bcda986d5b606ea4dd5522534906
Mark Soderquist <mark@soderquist.net>
2021-12-31 21:04:34 -0700

    Work on verify

--- src/main/java/com/desertskyrangers/flightlog/adapter/api/WebSecurity.java
+++ src/main/java/com/desertskyrangers/flightlog/adapter/api/WebSecurity.java
@@ -15,9 +15,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -56,35 +53,19 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure( HttpSecurity http ) throws Exception {
 		// @formatter:off
-		http
-			.cors().and()
-			.csrf().ignoringAntMatchers( ApiPath.AUTH_LOGIN ).and()
-			.csrf().ignoringAntMatchers( ApiPath.AUTH_REGISTER ).and()
-			.authorizeRequests()
+		http.cors()
+			.and().csrf().ignoringAntMatchers( ApiPath.AUTH_LOGIN )
+			.and().csrf().ignoringAntMatchers( ApiPath.AUTH_REGISTER )
+			.and().csrf().ignoringAntMatchers( ApiPath.AUTH_VERIFY )
+			.and().authorizeRequests()
 				.mvcMatchers( HttpMethod.POST, ApiPath.AUTH_LOGIN ).permitAll()
 				.mvcMatchers( HttpMethod.POST, ApiPath.AUTH_REGISTER ).permitAll()
-				.mvcMatchers( HttpMethod.GET, ApiPath.AUTH_VERIFY ).permitAll()
+				.mvcMatchers( HttpMethod.PUT, ApiPath.AUTH_VERIFY ).permitAll()
+				//.mvcMatchers( HttpMethod.GET, ApiPath.AUTH_VERIFY ).permitAll()
 				.mvcMatchers( HttpMethod.GET, ApiPath.MONITOR_STATUS ).permitAll()
 				.anyRequest().authenticated()
 			.and().addFilterAfter( new JwtFilter( jwtTokenProvider ), UsernamePasswordAuthenticationFilter.class );
 		// @formatter:on
-	}
-
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		final CorsConfiguration configuration = new CorsConfiguration();
-		//		configuration.setAllowedOrigins( List.of( "*" ) );
-		//		configuration.setAllowedMethods( List.of( "HEAD", "GET", "POST", "PUT", "DELETE", "PATCH" ) );
-		//		// setAllowCredentials(true) is important, otherwise:
-		//		// The value of the 'Access-Control-Allow-Origin' header in the response must
-		//		// not be the wildcard '*' when the request's credentials mode is 'include'.
-		//		configuration.setAllowCredentials( true );
-		//		// setAllowedHeaders is important! Without it, OPTIONS preflight request
-		//		// will fail with 403 Invalid CORS request
-		//		configuration.setAllowedHeaders( List.of( "Authorization", "Cache-Control", "Content-Type" ) );
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration( "/**", configuration );
-		return source;
 	}
 
 }
